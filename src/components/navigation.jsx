8a9cb9e3c8a8a0389930b52df3b5acc28806491d
Nikkolla <arebanikko@gmail.com>
2023-01-15 02:41:41 +0400

    logo name

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
   return (
     <div className="w-9/12 ml-auto mr-auto pt-3 pb-3 relative mb-10">
-        <div className="bg-black pl-2 p-1 absolute">
-          <img className="bg-black" src="https://cdn.shopify.com/s/files/1/0012/6569/6817/files/Untitled-4_1_350x.png?v=1613189550" />
+        <div className=" pl-2 p-1 absolute">
+          <img className="max-w-xs" src="https://i.imgur.com/b23nDJ1.png" />
       </div>
       <div className=" text-black">
         <div className="flex gap-20 text-lg justify-end pt-9">
+        <Link to="/">მთავარი</Link>
           <Link to="/macbook">MacBooks</Link>
           <Link to="/contact">კონტაქტი</Link>
           <Link to="/else">ჩვენს შესახებ</Link>
