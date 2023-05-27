54a17be851aacc22c2ad64b5ed32107ec47b4702
Nika <arebanikko@gmail.com>
2023-05-27 18:46:24 +0400

    mobile version updated

--- src/components/mobilemenu.jsx
+++ src/components/mobilemenu.jsx
@@ -1,38 +1,19 @@
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
-import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
-    <NavigationMenu.Root className="z-10">
-      <NavigationMenu.List>
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger>
-            <Icon icon="material-symbols:menu-rounded" color="black" width="28" height="30" />
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content>
-            <Link className="hover:border-b-2 hover:border-black hover:border-solid pb-1" to="/">
+    <div className="bg-[#2d2d2f] flex justify-between items-center text-white pt-5 px-8 pb-3">
+            <Link className="hover:border-b-2 hover:border-[#b9b9b9] hover:border-solid pb-1" to="/">
               მთავარი
             </Link>
             <br />
-            <Link className="hover:border-b-2 hover:border-black hover:border-solid pb-1" to="/macbook">
+            <Link className="hover:border-b-2 hover:border-[#b9b9b9] hover:border-solid pb-1" to="/macbook">
               პროდუქცია
             </Link>
             <br />
-            <Link className="hover:border-b-2 hover:border-black hover:border-solid pb-1" to="/contact">
+            <Link className="hover:border-b-2 hover:border-[#b9b9b9] hover:border-solid pb-1" to="/contact">
               კონტაქტი
             </Link>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Indicator className="z-10">
-          <div className="Arrow" />
-        </NavigationMenu.Indicator>
-      </NavigationMenu.List>
-
-      <div className="ViewportPosition">
-        <NavigationMenu.Viewport className="mt-3 w-full h-full bg-black text-white" />
-      </div>
-    </NavigationMenu.Root>
+    </div>
   );
 }
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -8,10 +8,10 @@ export default function Navigation() {
           <img className="max-w-xs invisible xl:visible w-0 h-0 xl:w-auto xl:h-auto" src="https://i.imgur.com/b23nDJ1.png" />
       </div>
       <div>
-        <div className="visible lg:invisible lg:w-0 lg:h-0 pt-9 lg:pt-0 lg:flex-none">
+        <div className="visible lg:invisible lg:w-0 lg:h-0 lg:pt-9 lg:flex-none">
         <Menu />
       </div>
-        <div className="flex gap-16 text-lg justify-end pt-9 invisible lg:visible w-0 h-0 lg:w-auto lg:h-auto text-black">
+        <div className="flex gap-16 text-lg justify-end lg:pt-9 invisible lg:visible w-0 h-0 lg:w-auto lg:h-auto text-black">
           <Link className="border-b-2 border-white hover:border-black hover:border-solid pb-1" to="/">მთავარი</Link>
           <Link className="border-b-2 border-white hover:border-black hover:border-solid pb-1" to="/macbook">პროდუქცია</Link>
           <Link className="border-b-2 border-white hover:border-black hover:border-solid pb-1" to="/contact">კონტაქტი</Link>
